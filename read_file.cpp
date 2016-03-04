a9704a2913de0c5cfb5dfabb9f7405bab148ae59
fredifqh <fredifqh@gmail.com>
2016-03-04 09:10:07 -0500

    Se ha creado la función Distance usando la estructura Point, para determinar la distancia entre dos puntos

--- read_file.cpp
+++ read_file.cpp
@@ -12,9 +12,13 @@
 
 using namespace std;
 
-//const double MASS_ELEC = 0.000510998928;
-//const double MASS_MUON = 0.105658369;
-//const double MASS_PHOT = 0;
+const double MASS_ELEC = 0.000510998928;
+const double MASS_MUON = 0.105658369;
+const double MASS_PHOT = 0;
+const double RADIO_ARRAY = 5;
+const double RADIO_DETEC = 1.5;
+const double COORD_DETEC_X = (sqrt (3)* RADIO_ARRAY)/2;
+const double COORD_DETEC_Y = 0.5*RADIO_ARRAY;
 
 double energy(double id_particle, double mass, double p_x, double p_y, double p_z){
 	
@@ -30,21 +34,36 @@ double energy(double id_particle, double mass, double p_x, double p_y, double p_
 	return sqrt(pow(mass, 2) + pow(p_x, 2) + pow(p_y, 2) + pow(p_z, 2));
 }
 
+struct Point
+{
+double e1, e2;
+};
+
+double Distance(const Point& p1, const Point& p2)
+{
+double x = p2.e1 - p1.e1 ;
+double y = p2.e2 - p1.e2 ;
+double r_sq = pow(x, 2) + pow(y, 2);
+return sqrt (r_sq);
+}
+
+
+
 double distance(double pos_x, double pos_y){
 	return pow(pos_x, 2) + pow(pos_y, 2);
 }
 
-struct resta_vectores{
-	double vectorA;
-	double vectorB;
-};
-
 int main(int argc, char const *argv[]){
-	
 
+
+	if (argc < 2)
+	{
+	cerr << "input the name of file\n"<< endl;
+	}
 	string program_name = argv[0];
 	//string file_name = argv[1];
 	
+
 	ifstream in(argv[1]);
 	ofstream out("result.dat");
 
@@ -72,11 +91,23 @@ int main(int argc, char const *argv[]){
 		}
 	}
 
+Point tanque_1 = {0, RADIO_ARRAY};
+Point tanque_2 = {-COORD_DETEC_X, -COORD_DETEC_Y};
+Point tanque_3 = { COORD_DETEC_X, -COORD_DETEC_Y};
+
+for (int i = 0; i < v.size(); i++) { 
+  Point position = {(v[i][4])/100, (v[i][5])/100};
+  if (Distance(position, tanque_1) < RADIO_DETEC || Distance(position, tanque_2) < RADIO_DETEC || Distance(position, tanque_3) < RADIO_DETEC)
+ out <<  v[i][4] << "    "<<v[i][5] << endl;
+}
+
+// los siguientes for se usan para leer toda la matriz
+/*
     for (int i = 0; i < v.size(); i++) { // i para fila             
         for (int j = 0; j < v[i].size(); j++) // j para columna
             out << v[i][j] << '\t';    
         out << endl;
     }
-
-    return 0;
+*/
+return 0;
 }
